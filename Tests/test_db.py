1b389ea63fada430035f6a4097f70649a6d406f1
sanchez yoan <yoan30470@hotmail.fr>
2021-05-14 11:44:26 +0200

    aaa

---
+++ Tests/test_db.py
@@ -0,0 +1,42 @@
+import unittest
+from main.py import read_plan
+
+TESTDATA_FILENAME = os.path.join(os.path.dirname(__file__), 'planning.log')
+
+self = "Exercice"
+start = "09:30"
+end = "11:15"
+
+class Test(unittest.TestCase): #ENTREE
+    
+    def UsersAcount(self):
+        self.testdata = open(TESTDATA_FILENAME).read()
+
+    def test_minutes(start, end):
+        start.assertEqual("09:00")
+        end.assertEqual("11:30")
+        start.assertIsInstance(int)
+        end.assertIsInstance(int)
+
+    def test_format(self):
+        self.assertFalse("09:20-11:00Introduction")
+        self.assertTrue("09:20-11:00 Introduction"
+        self.assertFalse("13:3014:10 Exercises")
+        self.assertTrue("13:30-14:10 Exercises")
+        self.assertFalse("9:30-10:30 Lists and Tuples")
+        self.assertTrue("09:30-10:30 Lists and Tuples")
+        self.assertFalse("Exercices 12:00-13:30")
+
+    def test_tuples_contains_two_numbers():
+        first_yield = return_first_yield()
+        for a_tuple in first_yield:
+            assert len(a_tuple) == 2
+    
+            for number in a_tuple:
+                assert isinstance(start, int)
+                assert isinstance(end, int)
+        
+    
+    
+    
+if__name__ == "__main__":unittest.main()
