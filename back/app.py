7a59c748d34c6b4c6a8dd790c9d03a96df2b3d55
StephanieR34 <34.regnier.stephanie@gmail.com>
2021-06-18 16:46:04 +0200

    test

--- back/app.py
+++ back/app.py
@@ -3,7 +3,9 @@ import logging
 import os
 from .functions import user_account
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def main():
@@ -13,4 +15,4 @@ def main():
 if __name__ == '__main__':
     logging.basicConfig(filename="prog.log", level=logging.DEBUG)
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0',port=port)
+    app.run(host='0.0.0.0', port=port)
